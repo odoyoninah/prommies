3c8efe41dce1b8e16ff82980999ea7fecf4cdaeb
odoyonina <odoyoninah@gmail.com>
2022-06-12 21:00:57 +0300

    create registration,login and signup files

---
+++ prommiesapp/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Prommies, Profile
+from dataclasses import fields
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class PrommiesForm(forms.ModelForm):
+    class Meta:
+        model = Prommies
+        fields = ['name', 'description', 'score', 'link', 'image', 'email', 'my_file']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'email', 'phone', 'address', 'image']
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
