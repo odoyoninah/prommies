2491ef56d38b4f805c8aa71fc6ea72ce93c31293
odoyonina <odoyoninah@gmail.com>
2022-06-14 20:49:39 +0300

    update database

--- prommies/settings.py
+++ prommies/settings.py
@@ -87,6 +87,7 @@ DATABASES = {
         'PASSWORD': 'Access',
         'HOST': 'localhost',
 
+
     }
 }
 
@@ -144,4 +145,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/'
+
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'login'
