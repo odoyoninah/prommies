53b7c73d585f298a62d8ef9cbd8bd5636dff8f7a
odoyonina <odoyoninah@gmail.com>
2022-06-15 01:00:49 +0300

    link css to index file

--- prommiesapp/serializer.py
+++ prommiesapp/serializer.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from .models import Prommies, Profile
 
-class PrommiesSerializer(serializers.Serializer):
+class PrommiesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prommies
         fields = ('name', 'description', 'score', 'image', 'email', 'url')
 
-class ProfileSerializer(serializers.Serializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('user', 'image', 'bio', 'birth_date', 'mobile', 'project')
