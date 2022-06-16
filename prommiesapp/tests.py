68ab61e287b23a58c3f9839ff4a72deffb9da708
odoyonina <odoyoninah@gmail.com>
2022-06-17 00:06:04 +0300

    add test files

--- prommiesapp/tests.py
+++ prommiesapp/tests.py
@@ -1,3 +1,30 @@
+from collections import UserDict
 from django.test import TestCase
 
+from prommiesapp.models import Prommies
+
 # Create your tests here.
+class PrommiesTest(TestCase):
+    def setUp(self):
+        self.user = UserDict.objects.create_user(username='testuser', password='12345')
+        self.prommies = Prommies.objects.create(name='test prommies', description='test description', score=0, image='images/default.png', email='g@gmail.com', url='www.google.com')
+
+    def test_save_prommies(self):
+        self.assertTrue(self.prommies.save_prommies())
+
+    def test_delete_prommies(self):
+        self.assertTrue(self.prommies.delete_prommies())
+
+    def test_update_prommies(self):
+        self.assertTrue(self.prommies.update_prommies(self.prommies))
+
+    def test_save_profile(self):
+        self.assertTrue(self.profile.save_profile())
+
+    def test_delete_profile(self):
+        self.assertTrue(self.profile.delete_profile())
+
+    def test_update_profile(self):
+        self.assertTrue(self.profile.update_profile(self.profile))
+
+
